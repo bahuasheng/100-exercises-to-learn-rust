c4fdae37cc765e70bf491904176a9d1392449d43
Stephen Ye <bahuasheng@users.noreply.github.com>
2024-05-30 20:09:14 +0800

    基础语法练习

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,16 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n <= 1 {
+        return 1;
+    }
+    let mut res = 1;
+    let mut tmp = n;
+    while tmp >= 1 {
+        res *= tmp;
+        tmp -= 1;
+    }
+    res
 }
 
 #[cfg(test)]
